ed1c4b894620adfdc5e1e43180f3f3eb879e98d4
Japhet Elias <japhet9@gmail.com>
2021-09-23 00:25:42 +0000

    animated input text typewrite style via setTimeout

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+let timer = 1000;
+for (const char of sentence) {
+  setTimeout(() => {
+    // print the char here
+    process.stdout.write(char);
+  }, timer) // <= 1s delay to make it noticeable. Can dial it down later.
+  timer += 50;
+}
+setTimeout(() => {
+  console.log();
+}, timer)
